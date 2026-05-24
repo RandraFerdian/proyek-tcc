312876fcdb4172a573cddbaf883a2a3db79a1fc9
RandraFerdian <ferdianrandra@gmail.com>
2026-05-24 16:21:43 +0700

    membuat branch untuk backend

--- frontend/src/constants/locations.js
+++
@@ -1,6 +0,0 @@
-export const KITCHEN_LOCATION = {
-  name: "Dapur Katering SPPG Sleman Seyegan Margomulyo",
-  shortName: "Dapur Katering",
-  lat: -7.7278916,
-  lng: 110.3121143,
-};
--- frontend/src/constants/packageTypes.js
+++
@@ -1,9 +0,0 @@
-export const PACKAGE_TYPES = [
-  "Makanan Berat",
-  "Minuman Sehat",
-  "Cemilan Sehat",
-  "Paket Diet",
-  "Buah & Salad",
-];
-
-export const getPackageType = (pkg) => pkg?.type || "Makanan Berat";
